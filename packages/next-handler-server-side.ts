90fde6af99432ca9df7b5d5c2a5bc1e432492a0c
ncphi <nolan@ncphi.com>
2021-04-04 16:34:05 -0300

    next-handler-server-side adds the userId to the context and adds the session to the page props

--- packages/next-handler-server-side.ts
+++ packages/next-handler-server-side.ts
@@ -6,9 +6,12 @@ import {
   GetServerSidePropsResult,
 } from "next"
 import serializeDates from "next-serialize-dates"
+import { getSession } from "next-auth/client"
+import { Session } from "next-auth"
 
 export type ServerSideHandlerContext = GetServerSidePropsContext & {
   prisma: PrismaClient
+  userId?: number
 }
 
 export type ServerSideHandlerCallback<P> = (
@@ -21,18 +24,34 @@ export function serverSideHandler<P = any, Q extends ParsedUrlQuery = any>(
   return async (context) => {
     const prisma = new PrismaClient()
 
-    let result = {}
+    let result: any = { props: {} }
+    let session: Session | null = null
 
     try {
-      result = await callback({ ...context, prisma })
+      session = await getSession(context)
+      const userId = await getUserId(prisma, session)
+
+      result = await callback({ ...context, prisma, userId })
     } catch (e) {
       console.error(e)
     } finally {
       prisma.$disconnect()
     }
 
-    return serializeDates(result)
+    return serializeDates({ ...result, props: { ...result.props, session } })
   }
+}
+
+async function getUserId(prisma: PrismaClient, session: Session | null) {
+  if (!session) return
+
+  const prismaSession = await prisma.session.findFirst({
+    where: { accessToken: session.accessToken },
+  })
+
+  if (!prismaSession) return
+
+  return prismaSession.userId
 }
 
 export default serverSideHandler
