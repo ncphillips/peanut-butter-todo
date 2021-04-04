143b7cfdd46986bd7c642db5f8e2ab13c5a15045
ncphi <nolan@ncphi.com>
2021-04-04 16:34:18 -0300

    next-handler-api adds the user id to the context

--- packages/next-handler-api.ts
+++ packages/next-handler-api.ts
@@ -1,9 +1,11 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
 import { PrismaClient } from "@prisma/client"
 import { serializeDates } from "next-serialize-dates"
+import { getSession } from "next-auth/client"
 
 export type ApiContext = {
   prisma: PrismaClient
+  userId?: number
 }
 
 export type Callback = (
@@ -15,8 +17,9 @@ export type Callback = (
 export function handler(cb: Callback): NextApiHandler {
   return async (req, res) => {
     const prisma = new PrismaClient()
-    const context = { prisma }
+    const context: ApiContext = { prisma }
     try {
+      context.userId = await getUserId(req, prisma)
       req.body = JSON.parse(req.body || "{}")
 
       const response = await cb(req, res, context)
@@ -29,6 +32,20 @@ export function handler(cb: Callback): NextApiHandler {
       prisma.$disconnect()
     }
   }
+}
+
+async function getUserId(req: NextApiRequest, prisma: PrismaClient) {
+  const session = await getSession({ req })
+
+  if (!session) return
+
+  const prismaSession = await prisma.session.findFirst({
+    where: { accessToken: session.accessToken },
+  })
+
+  if (!prismaSession) return
+
+  return prismaSession.userId
 }
 
 export default handler
